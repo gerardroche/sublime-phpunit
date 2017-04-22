a7ef3f9b1a865be4fd1cf5b65a69b69ed624e5e5
Gerard Roche <gerardroche@users.noreply.github.com>
2017-04-22 12:29:08 +0100

    Move syntax resources to res folder

--- plugin.py
+++ plugin.py
@@ -295,7 +295,7 @@ class PHPUnit():
             'file_regex': exec_file_regex(),
             'quiet': not is_debug(self.view),
             'shell': False,
-            'syntax': 'Packages/phpunitkit/test-results.hidden-tmLanguage',
+            'syntax': 'Packages/phpunitkit/res/text-ui-result.hidden-tmLanguage',
             'word_wrap': False,
             'working_dir': working_dir
         })
