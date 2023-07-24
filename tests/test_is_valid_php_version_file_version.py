e6b3d44e39c261c65bd1513afd527805650fc406
Gerard Roche <gerardroche@users.noreply.github.com>
2023-07-24 12:03:10 +0100

    Add headers

--- tests/test_is_valid_php_version_file_version.py
+++ tests/test_is_valid_php_version_file_version.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2023 Gerard Roche
+#
+# This file is part of PHPUnitKit.
+#
+# PHPUnitKit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PHPUnitKit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PHPUnitKit.  If not, see <https://www.gnu.org/licenses/>.
+
 from PHPUnitKit.tests import unittest
 
 from PHPUnitKit.plugin import is_valid_php_version_file_version
